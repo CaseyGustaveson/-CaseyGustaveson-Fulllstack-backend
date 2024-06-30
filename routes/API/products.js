1af7bafd06553b146ae8d2681e18803496168a3f
Casey Gustaveson <gustcase@isu.edu>
2024-06-30 12:07:55 -0500

    pushing to heroku

---
+++ routes/API/products.js
@@ -0,0 +1,34 @@
+const { PrismaClient } = require('@caseygustaveson/test-app');
+const prisma = new PrismaClient();
+const express = require('express');
+const router = express.Router();
+
+router.get("/products", async (req, res) => {
+    try {
+        const products = await prisma.product.findMany();
+        res.status(200).json(products);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+router.get('/products/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await prisma.product.findUnique({
+            where: {
+                id: parseInt(id),
+            },
+        });
+        if (!product) {
+            return res.status(404).json({ error: 'Product Not Found' });
+        }
+        res.status(200).json(product);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+module.exports = router;
