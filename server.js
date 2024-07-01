67370b201fc24065b71e8b0f6b3ea68059c2fbff
CaseyGustaveson <161667272+CaseyGustaveson@users.noreply.github.com>
2024-07-01 08:31:54 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const express = require('express');
-const app = express()
+const app = express();
+require('dotenv').config();
 const PORT = process.env.PORT || 3000;
-require('dotenv').config()
 
 const productRoutes = require('./routes/API/products');
+
+// Middleware
 app.use(express.json());
 
+// Routes
 app.use('/api', productRoutes);
 
 app.get('/', (req, res) => {
-    res.json({'message':'hello there'})
-})
+    res.json({ 'message': 'hello there' });
+});
+
+// Error handling
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-})
+});
