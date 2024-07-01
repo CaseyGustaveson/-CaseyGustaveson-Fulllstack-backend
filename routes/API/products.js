89abe98b7c005935f342b70a8254fadcb65c54dc
CaseyGustaveson <161667272+CaseyGustaveson@users.noreply.github.com>
2024-07-01 08:34:06 -0500

    Update products.js

--- routes/API/products.js
+++ routes/API/products.js
@@ -3,31 +3,39 @@ const prisma = new PrismaClient();
 const express = require('express');
 const router = express.Router();
 
-router.get("/products", async (req, res) => {
+router.get('/products', async (req, res) => {
     try {
         const products = await prisma.product.findMany();
-        console.log("products", products)
+        console.log('Fetched products:', products);
         res.status(200).json(products);
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Internal Server Error" });
+        console.error('Error fetching products:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
 });
 
 router.get('/products/:id', async (req, res) => {
     try {
         const { id } = req.params;
+        const productId = parseInt(id);
+        if (isNaN(productId)) {
+            return res.status(400).json({ error: 'Invalid Product ID' });
+        }
+
         const product = await prisma.product.findUnique({
             where: {
-                id: parseInt(id),
+                id: productId,
             },
         });
+
         if (!product) {
             return res.status(404).json({ error: 'Product Not Found' });
         }
+
+        console.log('Fetched product:', product);
         res.status(200).json(product);
     } catch (error) {
-        console.log(error);
+        console.error('Error fetching product:', error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
